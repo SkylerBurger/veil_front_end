7c1fa4a4ba38808d58697b5d7c9d1935344d0b6e
Skyler Burger <skylerburger@gmail.com>
2020-01-08 08:58:15 -0800

    Add onClick for card images

--- veil/src/components/position/position.js
+++ veil/src/components/position/position.js
@@ -3,17 +3,37 @@ import './position.css'
 class Position extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            currentImg: 'img/Back.jpg',
+            currentText: '',
+        }
+    }
+
+    handleClick = e => {
+        let newSource = '';
+        let newText = '';
+        if (this.state.currentImg === 'img/Back.jpg') {
+            newSource = this.props.imageSource;
+            newText = this.props.cardName;
+            this.setState({
+                currentImg: newSource,
+                currentText: newText,
+            });
+        }
     }
 
     render() {
         return (
             <div className="position">
                 <div className='card-border'>
-                    <img src='img/Back.jpg' className="card-image card-back" />
-                    {/* <img src='img/Back.jpg' className="card-image card-back" /> */}
+                    <img 
+                        src={this.state.currentImg} 
+                        className="card-image card-back" 
+                        onClick={this.handleClick}
+                    />
                 </div>
-                <h3 className='label'>Future</h3>
-                <h4>Card Back</h4>
+                <h3 className='label'>{this.props.positionName}</h3>
+                <h4>{this.state.currentText}</h4>
             </div>
         )
     }
--- veil/src/components/table/table.js
+++ veil/src/components/table/table.js
@@ -5,14 +5,37 @@ import './table.css';
 class Table extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            position_1: 'past',
+            position_2: 'present',
+            position_3: 'future',
+            image_1: 'img/1 The Magician.jpg',
+            image_2: 'img/5 Wands.jpg',
+            image_3: 'img/Ace Pentacles.jpg',
+            name_1: 'The Magician',
+            name_2: '5 of Wands',
+            name_3: 'Ace of Pentacles',
+        }
     }
 
     render() {
         return (
             <div className="table">
-                <Position />
-                <Position />
-                <Position />
+                <Position 
+                    positionName={this.state.position_1}
+                    imageSource={this.state.image_1}
+                    cardName={this.state.name_1}
+                />
+                <Position 
+                    positionName={this.state.position_2}
+                    imageSource={this.state.image_2}
+                    cardName={this.state.name_2}
+                />
+                <Position 
+                    positionName={this.state.position_3}
+                    imageSource={this.state.image_3}
+                    cardName={this.state.name_3}
+                />
             </div>
         )
     }
